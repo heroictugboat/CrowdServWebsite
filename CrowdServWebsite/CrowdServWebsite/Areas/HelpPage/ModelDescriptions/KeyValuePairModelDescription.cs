ca0adfc9f6c59eaa1a184095af64ce2e60d16143
heroictugboat <xxmatthewhowardxx@gmail.com>
2015-10-03 22:55:30 -0500

    setting up more

---
+++ CrowdServWebsite/CrowdServWebsite/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace CrowdServWebsite.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
